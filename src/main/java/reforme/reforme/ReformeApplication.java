e19f4dae43bce7ed33961a3a3d1ac7eb8ec256f3
amosi <forheidi@naver.com>
2024-03-31 22:20:57 +0900

    create project

---
+++ src/main/java/reforme/reforme/ReformeApplication.java
@@ -0,0 +1,13 @@
+package reforme.reforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reforme/reforme/ReformeApplicationTests.java
@@ -0,0 +1,13 @@
+package reforme.reforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
