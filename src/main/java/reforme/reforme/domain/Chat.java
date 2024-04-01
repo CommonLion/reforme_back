a19daa4bd4f7e389b51f2c30fa43e8c244f16a11
amosi <forheidi@naver.com>
2024-04-01 17:01:36 +0900

    feat: Update chat
    Chat 클래스에 누락되어있던 Getter와 Setter 어노테이션 추가

--- src/main/java/reforme/reforme/domain/Chat.java
+++ src/main/java/reforme/reforme/domain/Chat.java
@@ -1,11 +1,13 @@
 package reforme.reforme.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Chat {
 
     @Id @GeneratedValue
