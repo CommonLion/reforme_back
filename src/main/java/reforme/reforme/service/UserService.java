b0856b30888a716198039c04d1a0ceeb0fe1678e
amosi <forheidi@naver.com>
2024-05-14 22:45:49 +0900

    feat: Add signin
    로그인 기능 어찌저찌 얼레벌레 완성
    된건가이게맞나이딴김피탕이코드라니

--- src/main/java/reforme/reforme/service/UserService.java
+++ src/main/java/reforme/reforme/service/UserService.java
@@ -1,25 +1,40 @@
 package reforme.reforme.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reforme.reforme.dto.UserDto;
 import reforme.reforme.entity.User;
 import reforme.reforme.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 회원 가입
      */
     @Transactional
-    public String join(User user) {
+    public String join(UserDto userDto) {
+
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setNickname(userDto.getNickname());
+
         userRepository.save(user);
         return user.getId();
     }
@@ -43,5 +58,21 @@ public class UserService {
 
     public User findOne(String userId) {
         return userRepository.findOne(userId);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        List<User> findUsers = this.userRepository.findById(userId);
+        if (findUsers.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        User user = findUsers.get(0);
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if ("admin".equals(userId)) {
+            authorities.add(new SimpleGrantedAuthority("admin"));
+        } else {
+            authorities.add(new SimpleGrantedAuthority("user"));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getId(), user.getPassword(), authorities);
     }
 }
