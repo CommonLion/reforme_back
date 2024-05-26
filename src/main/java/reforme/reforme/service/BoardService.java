8a0e4e9eee8678332eecbc1a0a6142520e9ab6cd
CommonLion <tndud9548@kgu.ac.kr>
2024-05-26 19:22:01 +0900

    chore : modify Board ResponseBody BoardService의 메서드 타입을 ResponseBody<?>로 수정함.

--- src/main/java/reforme/reforme/service/BoardService.java
+++ src/main/java/reforme/reforme/service/BoardService.java
@@ -4,13 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 import reforme.reforme.dto.BoardDto;
 import reforme.reforme.dto.BoardUpdateDto;
+import reforme.reforme.dto.ResponseBody;
 
 
 //게시판 생성 수정 삭제 기능에 대한 서비스
 public interface BoardService {
     //GPT애서 나온거
     //ResponseEntity<?> createBoard(String title, String body, String category, MultipartFile[] images);
-    ResponseEntity createBoard(BoardDto boardDto, MultipartFile[] images, String repositoryType);
-    ResponseEntity updateBoard(Long boardId, BoardUpdateDto updateDto, MultipartFile[] images, String repositoryType);
-    ResponseEntity deleteBoard(Long boardId, String repositoryType);
+    ResponseBody<?> createBoard(BoardDto boardDto, MultipartFile[] images, String repositoryType);
+    ResponseBody<?> updateBoard(Long boardId, BoardUpdateDto updateDto, MultipartFile[] images, String repositoryType);
+    ResponseBody<?> deleteBoard(Long boardId, String repositoryType);
 }
