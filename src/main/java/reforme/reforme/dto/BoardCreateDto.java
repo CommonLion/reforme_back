8eb6edc78b84221eb30516fa9e8aa2dd3bbd5f32
CommonLion <tndud9548@kgu.ac.kr>
2024-05-28 17:57:34 +0900

    chore : modify dto name to BoardCreateDto dto 이름 변경 BoardDto to BoardCreateDto

--- src/main/java/reforme/reforme/dto/BoardCreateDto.java
+++ src/main/java/reforme/reforme/dto/BoardCreateDto.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
-public class BoardDto {
+public class BoardCreateDto {
     String title;   //작성한 제목
     String body;    //작성 내용
     String category;//카테고리
