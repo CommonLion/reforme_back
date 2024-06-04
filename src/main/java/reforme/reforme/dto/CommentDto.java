7f2756ee59a6eba6ff96a1671741eb1c10468ef9
player7571 <pxy7571@gmail.com>
2024-06-05 00:47:15 +0900

    chore : modify CommentDto
    CommentDto에 id 추가

--- src/main/java/reforme/reforme/dto/CommentDto.java
+++ src/main/java/reforme/reforme/dto/CommentDto.java
@@ -7,11 +7,14 @@ import reforme.reforme.entity.Comment;
 @Getter @Setter
 public class CommentDto {
 
+    private Long id;
+
     private String nickname;
 
     private String content;
 
     private Boolean secret;
+
 
     public CommentDto(String content, Boolean secret){
         this.content = content;
@@ -19,6 +22,7 @@ public class CommentDto {
     }
 
     public CommentDto(Comment comment){
+        this.id = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
         this.secret = comment.getSecret();
